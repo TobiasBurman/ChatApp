1bd1b0b79eaac42d692c666f58da8b15978a6fa3
Leon <xleonidasprox@gmail.com>
2024-08-27 16:35:06 +0200

    easy change

--- Social-Souls/src/App.jsx
+++ Social-Souls/src/App.jsx
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Social Souls ffs</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 300)}>
           count is {count}
         </button>
         <p>
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the dog for woof
       </p>
     </>
   )
