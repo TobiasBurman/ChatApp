57c2bcdd1870d431c41c4d1e75e0c8d1b1ff9159
Leon <xleonidasprox@gmail.com>
2024-08-29 01:46:55 +0200

    landing page beginning added

--- Social-Souls/src/Landing.jsx
+++ Social-Souls/src/Landing.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
-import './App.css'
+import './Landing.css'
+import {motion} from "framer-motion"
+import logotrans from "./assets/logo_trans.png"
+
+
+
+
 
 function App() {
 
 
   return (
     <>
-   <h1>Welcome to Social Souls</h1>
-     
+
+   <h1 id='header'>Welcome to <h2 class="header2">  Social Souls</h2></h1>
+   <div id="header_container">
+   <img src={logotrans} alt="logo" width="350x" height="250px"></img>
+   </div>
+
+   <p class="description">A dreadfully engaging social platform</p>
+   
+
     </>
   )
 }
