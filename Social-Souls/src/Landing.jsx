fb2b1897d881397f01961837e1035426354561dc
Leon <xleonidasprox@gmail.com>
2024-09-02 01:21:57 +0200

    login form added + bugs left

--- Social-Souls/src/Landing.jsx
+++ Social-Souls/src/Landing.jsx
@@ -12,15 +12,49 @@ function App() {
 
   return (
     <>
+        <div id="header_container">
+        <div className="text-container">
+          <h1 id='header'>Welcome to <span className="header2">Social Souls</span></h1>
+          <p className="description">A dreadfully engaging social platform</p>
+        </div>
+        <div className="image-container">
+    
+        </div>
+      </div>
 
-   <h1 id='header'>Welcome to <h2 class="header2">  Social Souls</h2></h1>
-   <div id="header_container">
-   <img src={logotrans} alt="logo" width="350x" height="250px"></img>
-   </div>
+      <div className="form-container">
+        {/* Checkbox used to toggle forms */}
+        <input type="checkbox" id="signup-toggle" />
 
-   <p class="description">A dreadfully engaging social platform</p>
-   
+        <form className="form">
+          {/* Login form (Front) */}
+          <div className="form_front">
+            <div className="form_details">Login</div>
+            <input placeholder="Username" className="input" type="text" />
+            <input placeholder='Password' className="input" type='password' />
+            <button type="button" className="btn">Login</button>
+            <span className="switch">
+              Don't have an account?
+              {/* Using htmlFor to link label to checkbox */}
+              <label className="signup_tog" htmlFor="signup-toggle">Sign up</label>
+            </span>
+          </div>
 
+          {/* Signup form (Back) */}
+          <div className="form_back">
+            <div className="form_details">SignUp</div>
+            <input placeholder='Username' className="input" type="text" />
+            <input placeholder='Password' className="input" type="password" />
+            <input placeholder='Confirm password' className="input" type="password" />
+            <button type="button" className="btn">SignUp</button>
+            <span className="switch">
+              Already have an account?
+              {/* Using htmlFor to link label to checkbox */}
+              <label className="signup_tog" htmlFor="signup-toggle">Login</label>
+            </span>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
